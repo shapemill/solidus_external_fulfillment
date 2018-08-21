a991113fc3f13d568bc914a2e9fde9398925c2e0
per <per@stuffmatic.com>
2018-08-21 13:24:13 +0200

    fix deface warning

--- app/overrides/fulfillment_centers_menu_tab.rb
+++ app/overrides/fulfillment_centers_menu_tab.rb
@@ -2,6 +2,7 @@ Deface::Override.new(
   virtual_path: "spree/admin/shared/_menu",
   name: "fulfillment_centers_admin_tab",
   insert_bottom: "[data-hook='admin_tabs']",
+  original: 'b7b242ee058673679949d7e3050be5974938294d',
   disabled: false) do
     <<-HTML
     <% if can? :admin, Spree::FulfillmentCenter %>
