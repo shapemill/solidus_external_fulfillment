2c0809ae89cf0428885871f3acd67e68b1cca083
per <per@stuffmatic.com>
2018-08-21 12:57:46 +0200

    use bootstrap css for external fulfillment request layout only

--- config/initializers/solidus_external_fulfillment.rb
+++ config/initializers/solidus_external_fulfillment.rb
@@ -1,2 +1,3 @@
 Spree::ExternalFulfillment.fulfillment_types = [:dummy_type_1, :dummy_type_2]
+Rails.application.config.assets.precompile += %w( external_fulfillment_request.css )
 Spree::ExternalFulfillment.fulfillment_request_notifier_class = "Spree::DefaultFulfillmentRequestNotifier"
