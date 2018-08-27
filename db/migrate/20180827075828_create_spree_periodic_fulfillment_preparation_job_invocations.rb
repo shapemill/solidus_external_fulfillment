8e2edc150963a530261b53a573f37c211060c05c
per <per@stuffmatic.com>
2018-08-27 13:11:39 +0200

    add index to periodic job invocation state column

--- db/migrate/20180827075828_create_spree_periodic_fulfillment_preparation_job_invocations.rb
+++ db/migrate/20180827075828_create_spree_periodic_fulfillment_preparation_job_invocations.rb
@@ -1,7 +1,7 @@
 class CreateSpreePeriodicFulfillmentPreparationJobInvocations < ActiveRecord::Migration[5.2]
   def change
     create_table :spree_periodic_fulfillment_preparation_job_invocations do |t|
-      t.integer :state, null: false, default: 0
+      t.integer :state, null: false, default: 0, index: { name: 'index_spree_periodic_fulfillment_prep_job_invocations_on_state' }
       t.integer :failed_count
       t.integer :reset_count
       t.integer :prepared_count
