8d4712848153cf720313a8f9ea07e3b019182c71
per <per@stuffmatic.com>
2018-09-10 19:36:46 +0200

    fix ship order partial override deface warning

--- app/overrides/custom_ship_order_partial.rb
+++ app/overrides/custom_ship_order_partial.rb
@@ -3,6 +3,7 @@ Deface::Override.new(
   name: "custom_ship_order_partial",
   replace: "erb[loud]:contains('form_tag(\"#\", class: \"admin-ship-shipment\")')",
   closing_selector: "erb[silent]:contains('end')",
+  original: 'd2ed1e8dd29f38e592c0ad682f8554e7d8ea4ca8',
   disabled: false) do
     <<-HTML
     <%= render partial: 'fulfillment', locals: { order: @order } %>
