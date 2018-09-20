c0d25870201348416292f8aee0d64bc983e38f5b
per <per@stuffmatic.com>
2018-09-20 11:27:22 +0200

    remove timestamps from line item fulfillment instructions

--- db/migrate/20180819193613_create_spree_line_item_fulfillment_instructions.rb
+++ db/migrate/20180819193613_create_spree_line_item_fulfillment_instructions.rb
@@ -1,7 +1,7 @@
 class CreateSpreeLineItemFulfillmentInstructions < ActiveRecord::Migration[5.2]
   def change
     create_table :spree_line_item_fulfillment_instructions do |t|
-      t.timestamps
+
     end
 
     add_reference :spree_line_item_fulfillment_instructions, :spree_fulfillment_request, index: { name: 'spree_line_item_fullfilment_instr_request_idx' }
